adac4d21d14fbf1dba3aa2df482d274dd1678a06
OscarAlex <43146741+OscarAlex@users.noreply.github.com>
2020-09-24 11:33:42 -0500

    Primera pantalla

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app= Flask(__name__)
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        return('archivo subido')
+
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
+
