1376f65626676fcb2baac1dbc5c2a074d7406f97
OscarAlex <43146741+OscarAlex@users.noreply.github.com>
2020-10-08 17:30:45 -0500

    Striped features table :(

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import pandas as pd
+import numpy as np
+from sklearn.experimental import enable_iterative_imputer  
+from sklearn.impute import IterativeImputer
+from sklearn.ensemble import ExtraTreesRegressor as ETR
 
 app= Flask(__name__)
 
@@ -25,6 +29,7 @@ def Add_CSV():
             #Get file
             fileSaved= fileLoaded.filename
             global data
+            data= pd.DataFrame()
             data= pd.read_csv(fileSaved)
             #Treeshold if NaN
             df= list(data.loc[:, data.isnull().mean() < .9].columns)
@@ -53,6 +58,7 @@ def Imput():
 @app.route('/add_imput', methods=['POST'])
 def Add_Imput():
     if request.method == 'POST':
+        
         #Keep/remove empty columns if exist
         if emptyCols:
             keep= request.form['empty']
@@ -62,10 +68,40 @@ def Add_Imput():
                 global data
                 data= data.loc[:, data.isnull().mean() < .9]
             
-        print(list(data.columns))
+        columns_name= list(data.columns)
+        print(columns_name)
         #Get apply imputation
         imput= request.form['imputation']
-        #print(imput)
+        print(imput)
+        #If apply imputation
+        if imput=='1':
+            #Separate columns numeric and no numeric
+            def splitTypes(dframe):
+                objectsN= []
+                for i in dframe.columns:
+                    #If i column is not  float
+                    if(not (dframe[i].dtype == np.float64)):
+                        #Get name
+                        objectsN.append(i)
+                        #Get no numeric column
+                        objects= dframe[i]
+                        #Drop no numeric column
+                        dframe= dframe.drop(columns=objectsN)
+                return dframe, objects
+            
+            x, y= splitTypes(data)
+            #Extra Tree Classifier
+            impute_est= ETR(n_estimators=10, random_state=0)
+            #Iterative imputer
+            estimator= IterativeImputer(random_state=0, estimator=impute_est)
+            #Fit transform data
+            impdf= estimator.fit_transform(x)
+            #Join imputed data and class
+            data= pd.concat([pd.DataFrame(impdf),y], axis=1)
+            #Rename columns
+            data.columns= columns_name
+
+        print(data)
         return redirect(url_for('Learning'))
 
 @app.route('/learning')
@@ -73,15 +109,53 @@ def Learning():
     return render_template('learning.html')
 
 @app.route('/choose_learn', methods=['POST'])
-def ChooseLearning():
+def Choose_Learning():
     if request.method == 'POST':
         learning= request.form['learning']
         print(learning)
+        #Return templates and column names
         if learning=='sup':
-            return render_template('supfeatures.html')
+            return render_template('supfeatures.html', features=list(data.columns))
         else:
-            return render_template('unsupfeatures.html')
+            return render_template('unsupfeatures.html', features=list(data.columns))
+
+@app.route('/add_supfeats', methods=['POST'])
+def ADD_SupFeats():
+    if request.method == 'POST':
+        #Get class selected
+        cl= int(request.form['class'])
+        print(data.columns[cl])
+        clas= data[data.columns[cl]]
+
+        #Get features selected
+        fe= request.form.getlist('features')
+        fe= [int(i) for i in fe] 
+        print(fe)
+
+        final_data= pd.DataFrame()
+        for i in fe:
+            final_data= pd.concat([final_data, data[data.columns[i]]], axis=1)
+        
+        print(final_data)
+        print(clas)
+        return redirect(url_for('Balance'))
+
+@app.route('/balance')
+def Balance():
+    return render_template('balance.html')
+
+
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
 
+"""
+@app.route('/add_supfeats', methods=['POST'])
+def ADD_SupFeats():
+    if request.method == 'POST':
+        return redirect(url_for('Balance'))
+
+@app.route('/balance')
+def Balance():
+    return render_template('balance.html')
+"""
--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,22 @@ tabs.forEach(tab => {
     })
 });*/
 
+function deactivate(id) {
+    //var clas= document.querySelector('input[name="class"]:checked').value;
+    var clasN= document.getElementById(id);
+    //var $radio = $('input[name="class"]:checked');
+    //var id = $radio.attr('id');
+    //console.log(clasN)
+
+    var checkbox1 = document.getElementById(id);
+    console.log("Checbox "+checkbox1)
+    checkbox1.disabled = true;
+
+    //var feat = document.getElementById("troncocomun");
+    //var primerelemento = document.getElementById("1").value;
+
+    //if(document.getElementById("1").value < 6) {
+      //  var checkbox1 = document.getElementById("checkbox2");
+       //checkbox1.disabled = true;
+    //}
+}
